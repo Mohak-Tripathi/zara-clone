7c005b790bb1560163b56f29b601959e4cb9b3b2
Rajuprajapti <rprajuprajapati16@gmail.com>
2022-05-04 11:31:02 +0530

    adding one file Best_seller for hard data

---
+++ Best_seller.js
@@ -0,0 +1,288 @@
+Best_seller_data = [
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1466/457/250/2/w/270/1466457250_2_1_1.jpg?ts=1649346739837",
+    name: "PRINTED DENIM JACKET",
+    price: "6590.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1466/456/406/2/w/270/1466456406_2_6_1.jpg?ts=1649764276875",
+    name: "DENIM JACKET WITH CONTRAST EMBROIDERY",
+    price: "6590.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0962/454/251/2/w/270/0962454251_2_1_1.jpg?ts=1649346724796",
+    name: "SKULL PRINT T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0962/454/800/2/w/270/0962454800_2_1_1.jpg?ts=1649346725616",
+    name: "SKULL PRINT T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1259/402/064/2/w/270/1259402064_1_1_1.jpg?ts=1649346728168",
+    name: "STRIPED MICKEY MOUSE © DISNEY T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1259/402/018/2/w/270/1259402018_1_1_1.jpg?ts=1649346744035",
+    name: "STRIPED MICKEY MOUSE © DISNEY T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/6224/449/251/2/w/270/6224449251_2_5_1.jpg?ts=1649411505195",
+    name: "SPRAY PRINT T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/6224/449/251/2/w/270/6224449251_1_1_1.jpg?ts=1649411495102",
+    name: "SPRAY PRINT T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/8416/400/427/2/w/270/8416400427_1_1_1.jpg?ts=1649411505725",
+    name: "PRINTED OVERSHIRT",
+    price: "4990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1187/475/406/2/w/270/1187475406_1_1_1.jpg?ts=1649764274571",
+    name: "SOFT DENIM BERMUDA SHORTS",
+    price: "2590.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1538/488/401/2/w/270/1538488401_2_4_1.jpg?ts=1649346743846",
+    name: "DENIM JUMPSUIT",
+    price: "5990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0840/436/406/2/w/270/0840436406_1_1_1.jpg?ts=1649152316451",
+    name: "RIPPED DENIM BERMUDA SHORTS WITH PAINT SPLATTER",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/3991/405/401/2/w/270/3991405401_1_1_1.jpg?ts=1649836931057",
+    name: "PAISLEY JACQUARD KNIT OVERSHIRT",
+    price: "4990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/4087/486/520/2/w/270/4087486520_2_1_1.jpg?ts=1648630686199",
+    name: "PREMIUM HOODIE",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1165/406/250/2/w/270/1165406250_2_1_1.jpg?ts=1648800286352",
+    name: "DISNEY PRINT T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1165/405/301/2/w/270/1165405301_2_1_1.jpg?ts=1648719801891",
+    name: "GARFIELD NICKELODEON T-SHIRT",
+    price: "1990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/3057/663/712/2/w/270/3057663712_1_1_1.jpg?ts=1649326313292",
+    name: "JACQUARD SHIRT",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/0706/416/330/3/w/270/0706416330_2_1_1.jpg?ts=1650299126582",
+    name: "GEOMETRIC JACQUARD SHIRT",
+    price: "6990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/7545/273/080/2/w/270/7545273080_2_5_1.jpg?ts=1649348297401",
+    name: "STRIPED TEXTURED SHIRT",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/2675/415/052/2/w/270/2675415052_1_1_1.jpg?ts=1649318045638",
+    name: "STRIPED TEXTURED SHIRT",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/4323/062/300/2/w/270/4323062300_2_1_1.jpg?ts=1650297898007",
+    name: "STRIPED OVERSHIRT",
+    price: "5990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/4516/854/712/2/w/270/4516854712_1_1_1.jpg?ts=1650297714687",
+    name: "SHIRT WITH GEOMETRIC TEXTURED WEAVE",
+    price: "5990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1300/308/526/2/w/270/1300308526_2_4_1.jpg?ts=1649318047934",
+    name: "TEXTURED BERMUDA SHORTS",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1300/308/526/2/w/270/1300308526_1_1_1.jpg?ts=1649318047208",
+    name: "TEXTURED BERMUDA SHORTS",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/1564/410/800/2/w/270/1564410800_1_1_1.jpg?ts=1640691938615",
+    name: "SLIM FIT SUIT BLAZER",
+    price: "5990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/1564/411/800/2/w/270/1564411800_2_4_1.jpg?ts=1640692475209",
+    name: "SLIM FIT TAILORED SUIT TROUSERS",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/4259/884/403/2/w/270/4259884403_1_1_1.jpg?ts=1648544084556",
+    name: "TEXTURED SUIT BLAZER",
+    price: "8990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/4259/884/403/2/w/270/4259884403_1_1_1.jpg?ts=1648544084556",
+    name: "TEXTURED SUIT BLAZER",
+    price: "8990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0029/404/401/2/w/270/0029404401_2_1_1.jpg?ts=1643300545623",
+    name: "PUFFER GILET",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0029/404/710/2/w/270/0029404710_2_3_1.jpg?ts=1643300547219",
+    name: "PUFFER GILET",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0029/404/800/2/w/270/0029404800_2_4_1.jpg?ts=1643300573139",
+    name: "PUFFER GILET",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/4391/403/505/2/w/270/4391403505_2_1_1.jpg?ts=1644568541332",
+    name: "RIPSTOP PUFFER GILET",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/0458/311/401/2/w/270/0458311401_2_1_1.jpg?ts=1646234909999",
+    name: "RIBBED KNIT VEST",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/3427/421/250/2/w/270/3427421250_2_1_1.jpg?ts=1645030474114",
+    name: "FAUX LEATHER PRINTED VEST WITH PATCHES",
+    price: "5990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/2795/400/505/2/w/270/2795400505_1_1_1.jpg?ts=1647532812822",
+    name: "PUFFER GILET WITH ZIP DETAIL",
+    price: "2890.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1608/406/800/2/w/270/1608406800_2_1_1.jpg?ts=1649152285388",
+    name: "TEXTURED WAISTCOAT",
+    price: "2890.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/2/p/1608/406/800/2/w/270/1608406800_2_1_1.jpg?ts=1649152285388",
+    name: "TEXTURED WAISTCOAT",
+    price: "2890.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/5372/425/067/3/w/270/5372425067_1_1_1.jpg?ts=1645789187209",
+    name: "CONTRAST CHECK JACKET",
+    price: "2890.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/5372/424/067/2/w/270/5372424067_2_4_1.jpg?ts=1645789250128",
+    name: "JOGGER TROUSERS WITH CONTRAST SIDE TAPING",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/0679/405/251/22/w/270/0679405251_15_1_1.jpg?ts=1643213899217",
+    name: "MARCO OGGIAN GRAPHIC TROUSERS",
+    price: "2990.00",
+  },
+
+  {
+    image:
+      "https://static.zara.net/photos///2022/V/0/1/p/0679/402/251/26/w/270/0679402251_1_1_1.jpg?ts=1642425487275",
+    name: "MARCO OGGIAN GRAPHIC TROUSERS",
+    price: "2990.00",
+  },
+];
