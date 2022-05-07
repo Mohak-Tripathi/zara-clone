e45a186c9156b5adacc0cdf31764ba8f9c93f1c9
stormyvikrant <vikrantmodern12@gmail.com>
2022-05-07 14:19:56 +0530

    update login signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+
+
+
+
 import FullPageExample from './components/LandingPage';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import PoloShirt from "./components/PoloShirt"
+
+ 
+
+
+
+
+
+
+
 import React, { useState } from 'react';
+
+
+
 import './App.css';
 import ProductPageZara from './components/ProductPageZara';
 import LinenPage from './components/LinenPage';
-import DetailPage from './components/DetailPage';
-import CardPayment from './components/card/CardPayment';
-import Payment from './components/Payment/Payment';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import CardPayment from './components/CardPayment';
+import Payment from './components/Payment';
 
 
 const App = () => {
@@ -24,26 +41,29 @@ const App = () => {
 
   return (
     <>
-
+   
 
 <Header openSidebar={toggleSidebar}/>  
 
 <Routes> 
-  
+
    <Route path="/ProductPage1" element={<ProductPageZara />}/> 
    <Route path="/HomePage" element={<FullPageExample  />}/> 
    <Route path="/LinenPage" element={<LinenPage  />}/> 
-   <Route path="/PoloPage" element={<PoloShirt  />}/> 
-   <Route path='/Polo_shirt/:id' element={<DetailPage/>}/>
-<<<<<<< HEAD
+   <Route path="/PoloPage" element={<PoloShirt  />}/>
+   <Route path="/login" element={<Login/>}/>
+   <Route path="/signup" element={<Signup/>}/>
    <Route path='/Payment' element={<Payment/>}/>
      <Route path='/CardPayment' element={<CardPayment/>}/>
-=======
-   <Route path="/" element={<FullPageExample/>}/>
->>>>>>> 7416a9505ad0a20c8f6193db742984f173a7a259
+  
+   
     </Routes>  
     
 <Sidebar sidebar={sidebar}/>
+
+
+
+
 
     </>
   );
